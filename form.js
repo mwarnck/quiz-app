28ba97d9b5dd1615545f6aae1724100d27d22aad
Mirco Warnck <mirco.warnck@googlemail.com>
2022-01-28 14:30:50 +0100

    Add form-js

---
+++ form.js
@@ -0,0 +1,18 @@
+const textareaQuestion = document.querySelector(
+  '[data-js="textarea-question"]'
+);
+const textareaAnswer = document.querySelector('[data-js="textarea-answer"]');
+const outputQuestion = document.querySelector('[data-js="output-question"]');
+const outputAnswer = document.querySelector('[data-js="output-answer"');
+
+textareaQuestion.addEventListener('input', () => {
+  outputQuestion.value = `${
+    200 - textareaQuestion.value.length
+  } characters remaining`;
+});
+
+textareaAnswer.addEventListener('input', () => {
+  outputAnswer.value = `${
+    200 - textareaAnswer.value.length
+  } characters remaining`;
+});
--- styles/components/form.stories.js
+++ styles/components/form.stories.js
@@ -4,16 +4,22 @@ export default {
 
 export const Default =
   () => `<form class="form w-375" action="" aria-labelledby="form-create-question">
-<label for="your-question">Your question:</label>
+<label for="your-question" class="form__label">Your question:</label>
+<output data-js="output-question" class="form__output">200 characters remaining</output>
 <textarea
+  data-js="textarea-question"
   id="your-question"
+  class="form__textarea"
   cols="10"
   rows="5"
   required
 ></textarea>
-<label for="your-answer">Your answer:</label>
+<label for="your-answer" class="form__label">Your answer:</label>
+<output data-js="output-answer" class="form__output">200 characters remaining</output>
 <textarea
+  data-js="textarea-answer"
   id="your-answer"
+  class="form__textarea"
   cols="10"
   rows="5"
   required
